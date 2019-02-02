cf398b3d5eeb883d353d14eddf7f3e536ccb0367
osmirmariano <osmirmariano@uft.edu.br>
2019-02-02 12:36:08 -0300

    Finalização do trabalho

--- Cliente.java
+++ Cliente.java
@@ -3,32 +3,33 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.util.Scanner;
 import java.net.Socket;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.util.Scanner;
 
 public class Cliente {
     public static void main(String[] args) throws UnknownHostException, IOException {
-        Socket cliente = new Socket("127.0.0.1", 12345);
-        System.out.println("O cliente se conectou ao servidor!");
+        File f = new File("/home/osmir/Documents/GitHub/pos/pos_SistemaDistribuido/distribuido.txt");
+        FileInputStream in = new FileInputStream(f);
 
-        diretorio = '/distribuido.txt';
-        System.out.println(diretorio);
-
-        output = new ObjectOutputStream(socket.getOutputStream());
-        File file = new File(diretorio);
-        FileInputStream arquivo = new FileInputStream(file);
-        int tamanho = (int)file.length();
-        output.writeInt(tamanho);
-        output.writeUTF(file.getName());
-        byte[] buffer = new byte[tamanho];
+        System.out.println("O cliente se conectou ao servidor! \n Diretorio do arquivo: " + f);
         
-        while(true){
-            int len = arquivo.read(buffer);
-            if(len == -1) break;
-            output.write(buffer, 0, len);
-            System.out.println(tamanho);
-            System.out.println("Arquivo enviado");
-        }
-
-        saida.close();
-        teclado.close();
+        Socket socket = new Socket("127.0.0.1", 1234);
+        OutputStream out = socket.getOutputStream();
+        OutputStreamWriter osw = new OutputStreamWriter(out);
+        BufferedWriter writer = new BufferedWriter(osw);
+        
+        writer.write(f.getName() + "\n");
+        writer.flush();
+        int tamanho = 4096; // buffer de 4KB  
+        byte[] buffer = new byte[tamanho];  
+        int lidos = -1;  
+        while ((lidos = in.read(buffer, 0, tamanho)) != -1) {  
+            out.write(buffer, 0, lidos);  
+        }   
     }
 }
--- Servidor.java
+++ Servidor.java
@@ -2,27 +2,40 @@ import java.net.ServerSocket;
 import java.io.IOException;
 import java.util.Scanner;
 import java.net.Socket;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Servidor {
   public static void main(String[] args) throws IOException {
-    ServerSocket servidor = new ServerSocket(12345);
-    System.out.println("Porta 12345 aberta!");
+   try {
+      ServerSocket server = new ServerSocket(1234);
+      Socket clSocket = server.accept();
+    
+      System.out.println("Nova conexao com o cliente " + clSocket.getInetAddress().getHostAddress());
 
-    Socket cliente = servidor.accept();
-    System.out.println("Nova conexao com o cliente " + cliente.getInetAddress().getHostAddress());
-
-    int tamanho = input.readInt();
-    String nomeArq = input.readUTF();
-    FileOutputStream file = new FileOutputStream("C:/Users/osmir/Documents/GitHub/POS/pos-SistemaDistribuido/"+nomeArq);
-    byte[] buffer = new byte[tamanho];
-    while(true){
-      int len = input.read(buffer);
-      if(len == -1) break;
-      file.write(buffer, 0, len);
+      InputStream in = clSocket.getInputStream();
+      InputStreamReader isr = new InputStreamReader(in);
+      BufferedReader reader = new BufferedReader(isr);
+      
+      File f1 = new File("/home/osmir/Documents/GitHub/pos/pos_SistemaDistribuido/distribuido_recebido.txt");
+      FileOutputStream out = new FileOutputStream(f1);
+          int tamanho = 1024; // buffer de 4KB  
+          byte[] buffer = new byte[tamanho];  
+          int lidos = -1;  
+          while ((lidos = in.read(buffer, 0, tamanho)) != -1) {  
+            System.out.println(lidos);
+              out.write(buffer, 0, lidos);  
+          }  
+          out.flush();  
+    } 
+    catch (IOException e) {
+      System.out.println("Erro No processamento " + e);
     }
-    System.out.println("Arquvio Recebido");
-
-    entrada.close();
-    servidor.close();
   }
 }
